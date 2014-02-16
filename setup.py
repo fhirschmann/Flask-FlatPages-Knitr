6169da9b4bb422c3c1a21c119b3d0bffb409cb70
Fabian Hirschmann <fabian@hirschm.net>
2014-02-16 02:13:19 +0100

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="Flask-FlatPages-Knitr",
-    version="0.1",
+    version="0.2",
     description="Knitr rendering for Flask-FlatPages",
     long_description=open(path.join(path.dirname(__file__), "README.rst")).read(),
     url="http://github.com/fhirschmann/Flask-FlatPages-Knitr",
